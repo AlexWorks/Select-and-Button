5cca801b10cfb807fb2c04edd2614486ccf004ee
AlexWorks <mister.lexx.ua@gmail.com>
2017-08-12 15:46:58 +0300

    Done with the custom button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomSelectComponent } from './custom-select/custom-select.component';
@@ -12,7 +13,8 @@ import { CustomizableButtonComponent } from './customizable-button/customizable-
     CustomizableButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/customizable-button/customizable-button.component.ts
+++ src/app/customizable-button/customizable-button.component.ts
@@ -12,4 +12,7 @@ export class CustomizableButtonComponent implements OnInit {
   ngOnInit() {
   }
 
+  button_text = "Sample Text";
+  button_image = "https://image.flaticon.com/icons/svg/121/121105.svg";
+
 }
