51b578fffef0330fd6c035fb9232c1be38d96dc5
AlexWorks <mister.lexx.ua@gmail.com>
2017-08-13 18:50:20 +0300

    Done with the custom select

--- src/app/custom-select/custom-select.component.ts
+++ src/app/custom-select/custom-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-custom-select',
@@ -8,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 
 export class CustomSelectComponent implements OnInit {
 
+
+  selectedOption: string;
+  isShown = false;
+
+  options: string[] = [
+    'Taco',
+    'Pizza',
+    'Burger',
+    'Burrito'
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.selectedOption = this.options[0];
   }
 
+  @HostListener('window:click', ['$event'])
+  onClick(event) {
+    if (this.isShown && !event.target.closest('.custom-select')) {
+      this.isShown = false;
+    }
+  }
+
+  toggle(): void {
+    this.isShown = !this.isShown;
+  }
+
+  onSelect(value: string): void {
+    this.selectedOption = value;
+    this.toggle();
+  }
+
+  onMouseDown(event: any): void {
+    event.preventDefault();
+  }
 }
